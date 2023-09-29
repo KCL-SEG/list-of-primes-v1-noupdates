6c81260f5fc79ce375f19b76a94aae09b9293183
that guy <user@DESKTOP-EOQPDVD.localdomain>
2023-09-29 01:10:22 +0100

    exercise 3

--- primes.py
+++ primes.py
@@ -1,6 +1,27 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(number):
+    if number<2:
+        return False
+    elif number==2 or number==3 or number==5 or number==7:
+        return True
+    else:
+        if number%2 !=0 and number%3 !=0 and number%5 !=0 and number%7 !=0 :
+            return True
+
 def primes(number_of_primes):
     list = []
+
+    i=0
+    while len(list) < number_of_primes:
+        if isPrime(i):
+            list.append(i)
+        i += 1
+        
+
     return list
+      
+
+        
+print(primes(20))
